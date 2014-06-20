ee9204da29f3d54bd91a0a82fe5b44613c2178a0
Yu Peng <loneknightpy@gmail.com>
2014-06-20 10:23:49 -0700

    Bump minor version and change contact email

--- config.h
+++ config.h
@@ -47,13 +47,13 @@
 #define PACKAGE "idba"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "ypeng@cs.hku.hk"
+#define PACKAGE_BUGREPORT "loneknightpy@gmail.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "idba"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "idba 1.1.1"
+#define PACKAGE_STRING "idba 1.1.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "idba"
@@ -62,13 +62,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.1"
+#define PACKAGE_VERSION "1.1.2"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.1"
+#define VERSION "1.1.2"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
